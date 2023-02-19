6c88b2b3a8e06a56882a188775f139ccbc149415
Coolguy6316 <coolguy6318@hotmail.com>
2023-02-19 11:15:06 +0200

    PlayerAnimator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
 
+    private bool isWalking;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -38,5 +40,15 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
